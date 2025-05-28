d58412cd4807ab0b427a4f7b0bf680bc6714fa7f
tomas <tomasgentile8@gmail.com>
2025-05-28 17:50:54 -0300

    feat(table): add functional table component

---
+++ client/src/components/Table.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import "./table.css";
+
+function Table({ dataTable }) {
+  const [bodyTable, setBodyTable] = useState([]);
+  const [headerTable, setHeaderTable] = useState([]);
+  useEffect(() => {
+    if (dataTable.length > 0) {
+      setBodyTable(getTableValues(dataTable));
+      setHeaderTable(getHeaderValues(dataTable));
+    }
+  }, [dataTable]);
+
+  useEffect(() => {
+    console.log(headerTable);
+  }, [headerTable]);
+  useEffect(() => {
+    console.log(bodyTable);
+  }, [bodyTable]);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headerTable.map((header, i) => {
+            return <th key={i}>{toDisplayFormat(header)}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {bodyTable.map((row, i) => {
+          return (
+            <tr key={i}>
+              {row.map((data, idx) => {
+                return <td key={i + String(idx)}>{data}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+function toDisplayFormat(str) {
+  return str
+    .replace(/([A-Z])/g, " $1")
+    .replace(/^./, (s) => s.toUpperCase())
+    .trim();
+}
+
+function getTableValues(dataTable) {
+  return dataTable.map((d) => {
+    return Object.values(d);
+  });
+}
+
+function getHeaderValues(dataTable) {
+  return Object.keys(dataTable[0]);
+}
+
+export default Table;
